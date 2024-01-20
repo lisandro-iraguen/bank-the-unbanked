d432702a60cb75adb9035c6d21fc1bfb790192ba
lisandro-iraguen <liraguen@est.frba.utn.edu.ar>
2024-01-20 13:28:59 -0300

    debug resource

--- Api/Services/Policy/PolicyManager.cs
+++ Api/Services/Policy/PolicyManager.cs
@@ -5,11 +5,10 @@ using CardanoSharp.Wallet.Models.Derivations;
 using CardanoSharp.Wallet.Models.Keys;
 using CardanoSharp.Wallet.TransactionBuilding;
 using CardanoSharp.Wallet.Utilities;
+using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using System;
 using Microsoft.Extensions.Configuration;
-using Azure.Identity;
-using Microsoft.AspNetCore.Mvc;
 namespace Api.Services.Policy;
 
 
@@ -38,23 +37,17 @@ public class PolicyManager : IPolicyManager
         _publicKey = paymentNode1.PublicKey;
     }
 
-    private string GetNmonicFromAPI(/*bool useKeyvault=false*/)
+    private string GetNmonicFromAPI()
     {
-       
-        try
-        {
-            string keyVaultUrl = _configuration["KeyVolt"];
-            var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
-            string secretName = _configuration["keyVoltScretName"];
-            KeyVaultSecret secret = client.GetSecret(secretName);
-            string nmonic = secret.Value;
-            return nmonic;
+            return "muffin brisk logic desk spot chase equal hen evil casual hat neck enemy since chief upon anxiety love stuff tent luggage chaos put winter";
+        
 
-        }catch(Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        return null;
+        //string keyVaultUrl = _configuration["KeyVolt"];
+        //var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+        //string secretName = _configuration["keyVoltScretName"];
+        //KeyVaultSecret secret = client.GetSecret(secretName);
+        //string nmonic = secret.Value;
+        //return nmonic;
     }
 
     public IScriptAllBuilder GetPolicyScript() =>
