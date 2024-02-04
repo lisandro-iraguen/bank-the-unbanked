233826ab549f509f06e09f287acb01e54d96c561
lisandro-iraguen <liraguen@est.frba.utn.edu.ar>
2024-02-04 10:13:03 -0300

    Web Developer Data Test , my First Test

---
+++ Test/WebDeveloperDataTests.cs
@@ -0,0 +1,32 @@
+using Api.WebData.Developers;
+
+namespace Test
+{
+    public class WebDeveloperDataTests
+    {
+        private IWebData webData;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            webData = new WebDeveloperData();
+
+        }
+
+        [Test]
+        public async Task GetWebDevelopersData_ReturnsExpectedData()
+        {
+            
+                
+                var result = await webData.GetWebDevelopersData();
+                var developersList = result.ToList();
+                Assert.That(developersList.Count, Is.EqualTo(1));
+                var developer = developersList.First();
+                Assert.That(developer.Text, Is.EqualTo("Lisandro Iraguen"));
+                Assert.That(developer.Url, Is.EqualTo("https://www.linkedin.com/in/lisandroiraguen/"));
+            
+        }
+    
+    }
+}
